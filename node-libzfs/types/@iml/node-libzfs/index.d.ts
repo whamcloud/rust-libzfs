dbb2a0ac3fbbae762ed1e6421736d5409ff81dd3
Joe Grund <joe.grund@intel.com>
2018-01-12 11:19:43 -0500

    Fixup TS declaration (#15)
    * Fixup TS declaration
    
    Update the tsdeclaration so it will be compiled properly by ts2fable.
    
    This will also require a PR in the ts2fable repo to land.
    
    Signed-off-by: Joe Grund <joe.grund@intel.com>

---
+++ node-libzfs/types/@iml/node-libzfs/index.d.ts
@@ -0,0 +1,62 @@
+// Copyright (c) 2017 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+declare module 'libzfs' {
+  export interface Dataset {
+    name: string;
+    kind: string;
+  }
+
+  export interface File {
+    path: string;
+  }
+
+  export interface Disk {
+    path: string;
+    phys_path?: string;
+    whole_disk?: boolean;
+    is_log: boolean;
+  }
+
+  export interface Mirror {
+    children: VDev[];
+    is_log?: boolean;
+  }
+
+  export interface RaidZ {
+    children: VDev[];
+  }
+
+  export interface Replacing {
+    children: VDev[];
+  }
+
+  export interface Spare {
+    children: VDev[];
+  }
+
+  export interface Root {
+    children: VDev[];
+  }
+
+  export type VDev = Mirror | RaidZ | Replacing | Spare | Root | Disk | File;
+
+  export interface Pool {
+    name: string;
+    uid: string;
+    state: string;
+    size: number;
+    vdev: VDev;
+    datasets: Dataset[];
+  }
+
+  export function getPoolByName(name: string): Pool | null;
+
+  export function getImportedPools(): Pool[];
+
+  export function getDatasetStringProp(
+    name: string,
+    prop: string
+  ): string | null;
+}
