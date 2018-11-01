32c3d50e523ac18c68d7f0de75e160e915f8af67
Joe Grund <jgrund@whamcloud.io>
2018-11-01 17:15:42 -0400

    make sure to emit config when building
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- libzfs-sys/build.rs
+++ libzfs-sys/build.rs
@@ -14,16 +14,16 @@ fn main() {
 
     let out_file = env::current_dir().unwrap().join("src").join("bindings.rs");
 
+    env::set_var("LIBCLANG_PATH", "/opt/llvm-5.0.0/lib64/");
+
+    pkg_config::Config::new().probe("libzfs").unwrap();
+    println!("cargo:rustc-link-lib=zpool");
+
     // Skip building if bindings already exist.
     // If you want to rebuild, delete the bindings file.
     if out_file.exists() {
         return;
     }
-
-    env::set_var("LIBCLANG_PATH", "/opt/llvm-5.0.0/lib64/");
-
-    pkg_config::Config::new().probe("libzfs").unwrap();
-    println!("cargo:rustc-link-lib=zpool");
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
