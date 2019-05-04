8f8250272171be192109a97a514268c701fa2a06
Joe Grund <jgrund@whamcloud.io>
2019-05-04 16:12:28 -0400

    Update libzfs-sys to 0.7.13
    Update bindings to 0.7.13.
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- libzfs-sys/src/lib.rs
+++ libzfs-sys/src/lib.rs
@@ -15,7 +15,7 @@
 //! to the src dir. To rebuild bindings run `cargo build`.
 //!
 //! ## ZFS version
-//! These bindings were compiled against ZFS 0.7.11. As `libzfs` is not a stable interface,
+//! These bindings were compiled against ZFS 0.7.13. As `libzfs` is not a stable interface,
 //! they should only be used against this version.
 //!
 //! ## OS
